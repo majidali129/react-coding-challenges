df78d4e9853ba8b8fd96f53f117ce89aa2ab70b4
majidali129 <itsmajidali129@gmail.com>
2025-02-23 10:41:27 +0500

    setup: porject setup done, integrated tailwindcss4

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="bg-amber-700 h-screen ">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button>Hi There</button>
+    </div>
+  );
+};
+
+export default App;
